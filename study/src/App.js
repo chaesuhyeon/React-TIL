8f5a87d8e920dd6e9257e2d3bf830abda9748b85
suhyeon <ddd8177@sweetk.co.kr>
2022-07-05 17:52:46 +0900

    생활코딩 리액트 6강

--- study/src/App.js
+++ study/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 
-function Header(props) {
-  return       <header>
-  <h1><a href='/'>{props.title}</a></h1>
-</header>
-}
 
 function Nav(props) {
   const lis = []
 
   for(let i=0;i<props.topics.length;i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+              }}>{t.title}</a>
+      </li>);
   }
   return       <nav>
   <ol>
@@ -26,18 +26,45 @@ function Article(props) {
   {props.body}
 </article>
 }
+
+function Header(props) {
+  return       <header>
+  <h1><a href='/' onClick={(event) => {
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
+</header>
+}
+
+
 function App() {
+  const mode = 'WELCOME';
+  // const mode = 'READ';
+
   const topics = [
     {id: 1, title :'html' , body :'html is ...'} ,
     {id: 2, title :'css' , body :'css is ...'} ,
     {id: 3, title :'js' , body :'js is ...'} 
 ]
+
+  let content = null;
+
+  if(mode === 'WELCOME') {
+      content = <Article title="Welcome" body="Hello, WEB"/>
+  } else if(mode === 'READ') {
+      content = <Article title="READ" body="Hello, WEB"/>
+  }
     
   return (
     <div>
-      <Header title="WEB"/>
-      <Nav topics={topics}/>
-      <Article title="Welcome" body="Hello, WEB"/>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}/>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      {content}
+      {/* <Article title="Welcome" body="Hello, WEB"/> */}
     </div>
   );
 }
