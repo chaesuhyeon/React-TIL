42075a528e419d080910289031eb6d7bb4c35198
suhyeon <ddd8177@gmail.com>
2022-07-06 10:49:58 +0900

    생활코딩 리액트 create

--- study/src/App.js
+++ study/src/App.js
@@ -38,15 +38,35 @@ function Header(props) {
 }
 
 
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+
+      // target : event를 발생시킨 태그 --> 여기서는 form
+      const title = event.target.title.value; // form 태그 -> input -> title의 value
+      const body = event.target.body.value; // form 태그 -> textarea -> body의 value
+
+      props.onCreate(title,body);
+    }}>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name='body' placeholder='body'></textarea></p>
+      <p><input type='submit' value='Create'/></p>
+    </form>
+  </article>
+}
+
 function App() {
 
   const[mode, setMode] = useState('WELCOME');
   const[id, setId] = useState('null');
-  const topics = [
+  const[nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id: 1, title :'html' , body :'html is ...'} ,
     {id: 2, title :'css' , body :'css is ...'} ,
     {id: 3, title :'js' , body :'js is ...'} 
-]
+])
 
   let content = null;
 
@@ -61,6 +81,17 @@ function App() {
       }
     }
       content = <Article title={title} body={body}/>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title , body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+
+    }}></Create>
   }
     
   return (
@@ -73,7 +104,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      {/* <Article title="Welcome" body="Hello, WEB"/> */}
+      <a href='/create' onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
