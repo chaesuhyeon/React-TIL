8a981aa8281acc8fdaac4f5c8aaaa68a454cf62a
suhyeon <ddd8177@gmail.com>
2022-07-12 18:10:30 +0900

    노마드코더 #7.6 진행중

--- nomad_react_beginner/react-for-beginners/src/components/Movie.js
+++ nomad_react_beginner/react-for-beginners/src/components/Movie.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
 
-export default function Movie({medium_cover_image, title, summary, genres}) {
+export default function Movie({id, medium_cover_image, title, summary, genres}) {
   return (
     <div>
     <img src={medium_cover_image} alt={title} />
-    <h2><Link to="/movie">{title}</Link></h2>
+    <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
     <p>{summary}</p>
     <ul>
         {genres.map((g)=> (
@@ -20,6 +20,7 @@ export default function Movie({medium_cover_image, title, summary, genres}) {
 }
 
 Movie.propTypes = {
+    id : PropTypes.number.isRequired,
     medium_cover_image : PropTypes.string.isRequired,
     title : PropTypes.string.isRequired, 
     summary : PropTypes.string.isRequired,
--- nomad_react_beginner/react-for-beginners/src/routes/Detail.js
+++ nomad_react_beginner/react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,9 @@
+import {useParams} from "react-router-dom"
+
 export default function Detail() {
+  const x = useParams();
+  console.log(x);
+  
   return (
     <h1>Detail</h1>
   )
