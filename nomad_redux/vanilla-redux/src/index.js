97ca6c91873c6358b91ce3869f61b70ad6e3d4c6
suhyeon <ddd8177@gmail.com>
2022-07-14 18:03:31 +0900

    노마드 코더 #1.2 진행중

--- nomad_redux/vanilla-redux/src/index.js
+++ nomad_redux/vanilla-redux/src/index.js
@@ -1,1 +1,19 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count=0 , action) =>{
+  count ++
+  count --
+  return count;
+};
+
+const countStore = createStore(countModifier) ;
+
+console.log(countStore.getState());
+
+countStore.dispatch({type : "ADD"})
+
 
