1c13460621803c36da10cf8c485e0d98f3bfdbd2
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-04-06 23:31:17 +0900

    feat: react portal 사용해서 장바구니 모달 추가

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -1,14 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showChartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideChartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
-        <main>
-            <Meals/>
-        </main>
+      {cartIsShown && <Cart onClose={hideChartHandler} />}
+      <Header onShowCart={showChartHandler} />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- udemy/react-complete-guide/src/components/UI/Input.js
+++ udemy/react-complete-guide/src/components/UI/Input.js
@@ -1,8 +1,12 @@
 import classes from "./Input.module.css";
 
-const Input = props => {
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input />
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} /> {/* input 속성들 다 넣어줌 */}
     </div>
-}
+  );
+};
+
+export default Input;
