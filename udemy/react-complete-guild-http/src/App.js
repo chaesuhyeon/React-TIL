75ce32689b83dc7b1812008fc73a0f7b3e49a774
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-04-17 19:23:05 +0900

    style: add description

--- udemy/react-complete-guild-http/src/App.js
+++ udemy/react-complete-guild-http/src/App.js
@@ -5,79 +5,82 @@ import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태
+  const [error, setError] = useState(null); // error 상태
 
-    const fetchMoviesHandler = useCallback(async () => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch("");
-            if (!response.ok) {
-                throw new Error("Something went wrong!");
-            }
+  // useCallback 함수 재사용
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("");
 
-            const data = await response.json();
+      // axios 를 사용하면 error 값도 return되지만 fetch는 지원하지 않아서 error 던짐
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-            const loadedMovies = [];
-            for (const key in data) {
-                loadedMovies.push({
-                    id: key,
-                    title: data[key].title,
-                    openingText: data[key].openingText,
-                    releaseDate: data[key].releaseDate,
-                });
-            }
+      const data = await response.json();
 
-            setMovies(loadedMovies);
-        } catch (error) {
-            setError(error.message);
-        }
-        setIsLoading(false);
-    }, []);
-
-    useEffect(() => {
-        fetchMoviesHandler();
-    }, [fetchMoviesHandler]);
-
-    async function addMovieHandler(movie) {
-        const response = await fetch("", {
-            method: "POST",
-            body: JSON.stringify(movie),
-            headers: {
-                "Content-Type": "application/json",
-            },
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
         });
-        const data = await response.json();
-        console.log(data);
+      }
+
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
     }
+    setIsLoading(false);
+  }, []);
 
-    let content = <p>Found no movies.</p>;
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-    if (movies.length > 0) {
-        content = <MoviesList movies={movies} />;
-    }
+  async function addMovieHandler(movie) {
+    const response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 
-    if (error) {
-        content = <p>{error}</p>;
-    }
+  let content = <p>Found no movies.</p>;
 
-    if (isLoading) {
-        content = <p>Loading...</p>;
-    }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
 
-    return (
-        <React.Fragment>
-            <section>
-                <AddMovie onAddMovie={addMovieHandler} />
-            </section>
-            <section>
-                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-            </section>
-            <section>{content}</section>
-        </React.Fragment>
-    );
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
 }
 
 export default App;
