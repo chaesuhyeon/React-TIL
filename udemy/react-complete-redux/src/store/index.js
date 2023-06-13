115f7b18d6b133ab7e32dbee29cbd82d1c7e27ef
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-06-13 22:29:30 +0900

    redux basic

---
+++ udemy/react-complete-redux/src/store/index.js
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter:
+        state.counter + action.amount /* 사용자 입력값 만큼 증가시킨다. */,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
+  return state; // if문을 안타게 될 경우에는 바뀌지 않은 상태를 리턴
+};
+
+const store = createStore(counterReducer);
+
+export default store;
